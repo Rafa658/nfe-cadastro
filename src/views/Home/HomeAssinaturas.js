bf5f4d627126d017dfcff39ac95035a0507d25ba
Rafael <rafollo@live.com>
2022-07-19 23:22:00 -0300

    Adicionado local de assinatura

--- src/views/Home/HomeAssinaturas.js
+++ src/views/Home/HomeAssinaturas.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 import styles from './Home.module.css'
 
@@ -8,22 +8,55 @@ export default function HomeAssinaturas() {
 
     const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
 
+    // implementação futura: definir cidade
     const [cidade, setCidade] = useState('')
+    const [resp, setResp] = useState('')
+    const [func, setFunc] = useState('')
+    // const [width, setWidth] = useState(0)
+    // const span = useRef()
+
+    // useEffect(() => {
+    //     setWidth(span.current.offsetWidth)
+    //     console.log(span.current.offsetWidth)
+    // }, [cidade])
 
     return (
         <div className={styles.Assinaturas}>
             <div className={styles.local}>
-                <input
+                {/* <input
                     className={styles.Input}
+                    style={{width}}
                     type='text'
                     name='cidade'
                     placeholder='cidade'
                     value={cidade}
                     onChange={e => setCidade(e.target.value)}
-                />
-                , {hoje.getDate()} de {meses[hoje.getMonth()]} de {hoje.getFullYear()}
+                    ref={span}
+                /> */}
+                Fortaleza, {hoje.getDate()} de {meses[hoje.getMonth()]} de {hoje.getFullYear()}
             </div>
-            <div className={styles.assinatura}>...</div>
+            <div className={styles.assinatura}>
+                <div className={styles.nomeDoResponsavel}>
+                    <input
+                        className={styles.Input}
+                        type='text'
+                        name='resp'
+                        placeholder='Responsável'
+                        value={resp}
+                        onChange={e => setResp(e.target.value)}
+                    />
+                </div>
+                <div className={styles.funcaoDoResponsavel}>
+                    <input
+                        className={styles.Input}
+                        type='text'
+                        name='func'
+                        placeholder='func'
+                        value={func}
+                        onChange={e => setFunc(e.target.value)}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
